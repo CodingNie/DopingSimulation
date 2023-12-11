f095769e4235eb7c56b4e581cf04a82a0180fdcc
CodingNie <niening1643@gmail.com>
2023-12-10 17:14:22 -0800

    implemented the MD.cpp

--- MD.cpp
+++ MD.cpp
@@ -1,31 +1,120 @@
 #include <mpi.h>
 #include <omp.h>
 #include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Constants and global variables
+const int numParticles = 10000; // Number of particles
+const double sigma = 1.0; // Lennard-Jones parameter for particle size
+const double epsilon = 1.0; // Lennard-Jones parameter for depth of potential well
+const double dt = 0.001; // Time step for integration
+const int num_steps = 1000; // Number of simulation steps
+const int blocks = 256; // Number of blocks for CUDA
+const int threads = 256; // Number of threads per block
+
+double positions[numParticles][3]; // Positions of particles
+double velocities[numParticles][3]; // Velocities of particles
+double forces[numParticles][3]; // Forces on particles
+
+// Function to calculate Lennard-Jones force between two particles
+void calculateLJForce(double pos1[3], double pos2[3], double force[3]) {
+    double r[3], distSquared = 0.0, distSixth, distTwelfth, fMagnitude;
+    for (int i = 0; i < 3; ++i) {
+        r[i] = pos1[i] - pos2[i];
+        distSquared += r[i] * r[i];
+    }
+    distSixth = distSquared * distSquared * distSquared;
+    distTwelfth = distSixth * distSixth;
+    fMagnitude = 24 * epsilon * (2 * pow(sigma, 12) / distTwelfth - pow(sigma, 6) / distSixth) / distSquared;
+
+    for (int i = 0; i < 3; ++i) {
+        force[i] = fMagnitude * r[i];
+    }
+}
 
 // Function to calculate forces
 void calculateForces() {
-    // Force calculation
+    // Initialize forces to zero
+    for (int i = 0; i < numParticles; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            forces[i][j] = 0.0;
+        }
+    }
+
+    // Calculate forces using Lennard-Jones potential
+    double force[3];
+    for (int i = 0; i < numParticles; ++i) {
+        for (int j = i + 1; j < numParticles; ++j) {
+            calculateLJForce(positions[i], positions[j], force);
+            for (int k = 0; k < 3; ++k) {
+                forces[i][k] += force[k];
+                forces[j][k] -= force[k]; // Newton's third law
+            }
+        }
+    }
 }
 
-// CUDA Kernel for force calculations
-__global__ void calculateForcesCUDA() {
-    // CUDA-based force calculation
+// CUDA Kernel for force calculations using Lennard-Jones potential
+__global__ void calculateForcesCUDA(double* positions, double* forces, int numParticles) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= numParticles) return;
+
+    double pos[3] = {positions[idx * 3], positions[idx * 3 + 1], positions[idx * 3 + 2]};
+    double force[3] = {0.0, 0.0, 0.0};
+
+    for (int i = 0; i < numParticles; ++i) {
+        if (i == idx) continue; // Skip self
+
+        double r[3], distSquared = 0.0, distSixth, distTwelfth, fMagnitude;
+        double pos_i[3] = {positions[i * 3], positions[i * 3 + 1], positions[i * 3 + 2]};
+
+        // Calculate distance between particles
+        for (int j = 0; j < 3; ++j) {
+            r[j] = pos[j] - pos_i[j];
+            distSquared += r[j] * r[j];
+        }
+
+        // Lennard-Jones potential calculations
+        distSixth = distSquared * distSquared * distSquared;
+        distTwelfth = distSixth * distSixth;
+        fMagnitude = 24 * epsilon * (2 * pow(sigma, 12) / distTwelfth - pow(sigma, 6) / distSixth) / distSquared;
+
+        // Accumulate force
+        for (int j = 0; j < 3; ++j) {
+            force[j] += fMagnitude * r[j];
+        }
+    }
+
+    // Write the calculated force back to global memory
+    for (int j = 0; j < 3; ++j) {
+        forces[idx * 3 + j] = force[j];
+    }
 }
+
 
 // Function to initialize the molecular system
 void initializeSystem() {
-    // Initialize positions, velocities, etc.
+    // Initialize positions and velocities of particles
     // This mainly contains Nitrogen, Boron, Carbon, and LFP particles
+    // For simplicity, I'll place particles randomly and assign random velocities
+    for (int i = 0; i < numParticles; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            positions[i][j] = (double)rand() / RAND_MAX; // Random position
+            velocities[i][j] = (double)rand() / RAND_MAX - 0.5; // Random velocity
+        }
+    }
 }
 
-// Function to distribute initial conditions to worker nodes
-void distributeInitialConditions() {
-    // Logic to distribute initial conditions to worker nodes
-}
-
-// Function to gather results from worker nodes
-void gatherResults() {
-    // Logic to gather results from worker nodes
+// Function to update positions and velocities
+void updateParticles() {
+    for (int i = 0; i < numParticles; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            positions[i][j] += velocities[i][j] * dt + 0.5 * forces[i][j] * dt * dt;
+            velocities[i][j] += forces[i][j] * dt;
+        }
+    }
 }
 
 int main(int argc, char** argv) {
@@ -33,36 +122,16 @@ int main(int argc, char** argv) {
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    // Differentiate between master and worker nodes
-    if (world_rank == 0) {
-        // Master node work
-        initializeSystem();
-        distributeInitialConditions();
-    } else {
-        // Worker nodes work
-        // Receive initial conditions from master node
+    // Initialize the molecular system
+    initializeSystem();
 
-
-#pragma omp parallel
-        {
-            // Parallel force calculations using OpenMP
-            calculateForces();
-        }
-
-        // CUDA kernel call for computationally intensive parts
-        calculateForcesCUDA<<<blocks, threads>>>();
-    }
-
-    // Time integration loop
+    // Main simulation loop
     for (int step = 0; step < num_steps; step++) {
-        // Update positions and velocities of molecules
+        // Calculate forces
+        calculateForces();
 
-        // Worker nodes send partial results to master node
-
-        if (world_rank == 0) {
-            // Master node gathers results from all worker nodes
-            gatherResults();
-        }
+        // Update positions and velocities of particles
+        updateParticles();
     }
 
     MPI_Finalize();
